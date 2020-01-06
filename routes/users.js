5857fd6ebf35ee906728785f8feb8db3d5a369f7
rue-elwahishi <rubawahishi@gmail.com>
2020-01-06 18:58:49 +0100

    User model CRUD

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require("express");
+const {
+  getUser,
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser
+} = require("../controllers/users");
 
 const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json({msg: 'hello'})
-});
-
-
+router
+  .route("/")
+  .get(getUsers)
+  .post(createUser);
+router
+  .route("/:id")
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
