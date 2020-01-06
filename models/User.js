b6fe6dcb7c63e9344dce1d33471ad50d8716c96b
Ammar Mohammed <safeerlibya@gmail.com>
2020-01-06 17:17:06 +0100

    added user schema

--- models/User.js
+++ models/User.js
@@ -2,6 +2,33 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     //User schame
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        match: [],
+        required: [true, 'Please add a valid email']
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
 });
 
 
