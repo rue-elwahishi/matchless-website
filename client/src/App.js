7d8fac92575662a5dc1fb5bcb5bbb69edb777a2d
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-08 14:59:28 +0100

    adding collection-item/ collection-preview with basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import {Switch, Route } from 'react-router-dom'
 
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import SectionPage from "./pages/section/section.component";
 
 
 function App() {
     return (
         <Switch>
             <Route exact path="/" component={HomePage} />
+            <Route exact path="/section" component={SectionPage} />
+
         </Switch>
     );
 }
