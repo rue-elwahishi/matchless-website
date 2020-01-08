54f7306a5bb8cf1a7d380f7c97bb2167eae3d285
rue-elwahishi <rubawahishi@gmail.com>
2020-01-08 20:09:56 +0100

    Authentication with Redux

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
+
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("http://localhost:3000/api/v1/users/register ", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("http://localhost:5002/api/v1/auth/login", userData)
+    .then(res => {
+      console.log(res);
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+// User loading
+export const setUserLoading = () => {
+  return {
+    type: USER_LOADING
+  };
+};
+export const logoutUser = () => dispatch => {
+  // Remove token from local storage
+  localStorage.removeItem("jwtToken");
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to empty object {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};
