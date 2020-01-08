7e6caf5754372ec2f321ea20da3a6b332131b256
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-08 15:34:47 +0100

    adding header component and a logo/ with link routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import {Switch, Route } from 'react-router-dom'
 
 import './App.css';
+
 import HomePage from "./pages/homepage/homepage.component";
 import SectionPage from "./pages/section/section.component";
+import Header from "./components/header/header.component";
 
 
 function App() {
     return (
-        <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/section" component={SectionPage} />
-
-        </Switch>
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/section" component={SectionPage} />
+            </Switch>
+        </div>
     );
 }
 
