47efb283ab3178bb68630160aac463682347e9d7
rue-elwahishi <rubawahishi@gmail.com>
2020-01-10 17:24:22 +0100

    added cart-icon w/toggle

--- client/src/components/header/header.component.js
+++ client/src/components/header/header.component.js
@@ -1,17 +1,21 @@
 import React, { Fragment } from "react";
 import { ReactComponent as Logo } from "../../assets/pijama.svg";
-
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 //redux related
 import { connect } from "react-redux";
 import { logout } from "../../actions/auth";
 import PropTypes from "prop-types";
 
 import { Link } from "react-router-dom";
-import SearchPage from "./../../components/search/search.component";
 
 import "./header.styles.scss";
 
-const Header = ({ auth: { isAuthenticated, loading }, logout }) => {
+const Header = ({
+  auth: { isAuthenticated, loading },
+  logout,
+  cart: { hidden }
+}) => {
   const authLinks = (
     <div className="options">
       <Link className="option" to="/section">
@@ -26,6 +30,7 @@ const Header = ({ auth: { isAuthenticated, loading }, logout }) => {
         {" "}
         LOGOUT{" "}
       </Link>
+      <CartIcon />
     </div>
   );
   const guestLinks = (
@@ -46,6 +51,7 @@ const Header = ({ auth: { isAuthenticated, loading }, logout }) => {
         {" "}
         SIGN UP{" "}
       </Link>
+      <CartIcon />
     </div>
   );
 
@@ -54,10 +60,11 @@ const Header = ({ auth: { isAuthenticated, loading }, logout }) => {
       <Link className="logo-container" to="/">
         <Logo height="70px" width="70px" className="logo" />
       </Link>
-      <SearchPage />
+      {/* <SearchPage /> */}
       {!loading && (
         <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
       )}
+      {hidden ? null : <CartDropDown />}
     </div>
   );
 };
@@ -68,7 +75,8 @@ Header.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
+  cart: state.cart
 });
 
 export default connect(mapStateToProps, { logout })(Header);
