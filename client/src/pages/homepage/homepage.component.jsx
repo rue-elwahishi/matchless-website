3cb1b6871a937656901fedcfc7e121ef79c3f285
rue-elwahishi <rubawahishi@gmail.com>
2020-01-10 01:57:20 +0100

    carousel done

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -2,48 +2,10 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 import Carousel from "react-bootstrap/Carousel";
-
+import Slider from "../../components/slider/slide.component";
 const HomePage = () => (
   <div className="homepage">
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.ibb.co/JppJ77L/hats.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.ibb.co/1sBwzTd/men.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3> MENS </h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.ibb.co/x7r6Ngn/mens.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <Slider />
     <Directory />
   </div>
 );
