03acc27895db2715cd03e07a0f276cdaff94f46a
rue-elwahishi <rubawahishi@gmail.com>
2020-01-11 13:39:02 +0100

    checkout item component implemented

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,23 +5,32 @@ import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../selectors/cart";
 import { withRouter } from "react-router-dom";
+import { toggleCartHidden } from ".././../actions/cart";
 
-const CartDropDown = ({ cartItems, history }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.length ? (
-        cartItems.map(cartItem => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))
-      ) : (
-        <span className="empty-message"> Your cart is empty </span>
-      )}
+const CartDropDown = ({ cartItems, history, dispatch }) => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message"> Your cart is empty </span>
+        )}
+      </div>
+      <CustomButton
+        onClick={() => {
+          dispatch(toggleCartHidden());
+          history.push("/checkout");
+        }}
+      >
+        CHECKOUT
+      </CustomButton>
     </div>
-    <CustomButton onClick={() => history.push("/checkout")}>
-      CHECKOUT
-    </CustomButton>
-  </div>
-);
+  );
+};
+
 const mapStateToProps = state => ({
   cartItems: selectCartItems(state)
 });
