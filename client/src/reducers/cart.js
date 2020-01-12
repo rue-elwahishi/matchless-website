0e629f7b6e41dff9325fc3b11eca05b836d935f2
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-12 16:20:58 +0100

    fixed checkout clear bug

--- client/src/reducers/cart.js
+++ client/src/reducers/cart.js
@@ -26,7 +26,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: state.cartItems.filter(
-          cartItem => cartItem.id !== action.payload.id
+          cartItem => cartItem._id !== action.payload._id
         )
       };
 
